fc76313164d59d7f8b496ea5d1ea9eb6b6d17fe2
mutha pavani <muttapavani9gmail.com>
2025-09-22 15:37:43 +0530

    added admin credentials

--- Bookings/apps.py
+++ Bookings/apps.py
@@ -1,9 +1,19 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
 
 class BookingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Bookings'
 
     def ready(self):
+        # Import your signals as before
         import Bookings.signals
+
+        # Create superuser if it doesn't exist
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='pavani',
+                email='pavani@example.com',
+                password='123456'
+            )
